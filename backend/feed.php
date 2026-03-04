e9f3faf565c4a211b2c31f670afc271ae63d2056
AlvinoP <102621060+Alvinopingvino@users.noreply.github.com>
2026-03-04 10:33:34 +0100

    Sök-funktion
    Gjorde så att användaren kan söka bland inlägg och få fram de som matchar bäst med dess sökning.

--- backend/feed.php
+++ backend/feed.php
@@ -2,13 +2,28 @@
 include('db_connect.php');
 include_once('base_url.php');
 
-$stmt = $dbconn->query('SELECT * FROM posts');
-$posts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+if (isset($_GET['search'])){
+    $stmt = $dbconn->prepare("
+    SELECT *,
+    (title LIKE ?) AS exact_score,
+    MATCH(title, content) AGAINST(? IN BOOLEAN MODE) AS score
+    FROM posts
+    ORDER BY exact_score DESC, score DESC");
+
+    $stmt -> execute([$_GET['search'], $_GET['search']]);
+    $posts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+else{
+    $stmt = $dbconn->query('SELECT * FROM posts');
+    $posts = $stmt->fetchAll(PDO::FETCH_ASSOC); 
+}
+
+
 
 foreach ($posts as $post) {
 
     $stmt = $dbconn->query("SELECT * FROM users WHERE id = {$post['author']}");
-    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);   
 
     echo ('
         <a style="color: black; text-decoration: none;" href="' . BASE_URL . 'frontend/php/post.php?post_id=' . $post['id'] . '">
@@ -22,8 +37,9 @@ foreach ($posts as $post) {
                         <h3 style="margin:0px"> ' . $post['title'] . '</h3>
                     </div>
                 </div>
-                <p> ' . $post['content'] .  ' </p>
+                <p class="post_content"> ' . $post['content'] .  ' </p>
             </div>
         </a>
+        <HR>
         ');
 }
